3f58bbda334a0b0c8b573596981e41293b38d0bb
Facundo López Llopis <faculopllop@gmail.com>
2023-05-31 13:55:21 -0300

    ranking corrections

--- resources/js/Pages/Ranking.vue
+++ resources/js/Pages/Ranking.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
-const textColors = ['!text-yellow-300', '!text-gray-100', '!text-orange-400'];
+import PrimaryButton from '../Components/PrimaryButton.vue';
+const textColors = ['!text-yellow-300', '!text-white', '!text-orange-400'];
 </script>
 
 <script>
@@ -20,6 +21,9 @@ const textColors = ['!text-yellow-300', '!text-gray-100', '!text-orange-400'];
                     let index = this.ranking.findIndex((part) => part.id == ranking[i].id);
                     if (index != -1) this.top[index] = (i-index) * 116.6;
                 }
+            },
+            goBack: function () {
+                window.history.back();
             }
         },
         mounted() {
@@ -50,9 +54,9 @@ const textColors = ['!text-yellow-300', '!text-gray-100', '!text-orange-400'];
     <div class="dark:bg-gray-800 absolute h-full w-full">
         <h1 class="mt-5 text-3xl text-center dark:text-gray-200">Ranking</h1>
         <div class="mt-5 mx-1 sm:mx-5 md:max-w-screen-lg md:mx-auto">
-            <div class="w-full">
+            <div class="w-full px-2">
                     <div class="relative ranking-item flex justify-between content-center p-2 mt-2 w-full dark:bg-gray-700 bg-gray-300 rounded" :style="top[i] && `transition: all 250ms; transform: translateY(${top[i]}%)`" v-for="(participant, i) in ranking">
-                    <h1 class="dark:text-gray-200 text-xl ranking-item-order" :class="i >= 0 && i <= 2 ? textColors[i] : ''">{{ i + 1 }}. {{ participant.name }}</h1>
+                    <h1 class="text-ellipsis overflow-hidden dark:text-gray-200 text-xl ranking-item-order" :class="i >= 0 && i <= 2 ? textColors[i] : ''">{{ i + 1 }}. {{ participant.name }}</h1>
                     <div class="flex justify-center content-center h-full">
                         <div class="dark:bg-gray-800 rounded p-1 w-12">
                             <h1 class="dark:text-gray-200 text-center">{{ participant.points }}</h1>
@@ -60,6 +64,9 @@ const textColors = ['!text-yellow-300', '!text-gray-100', '!text-orange-400'];
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="flex w-full justify-center mt-4">
+            <PrimaryButton @click="goBack()">Volver</PrimaryButton>
         </div>
     </div>
 </template>
